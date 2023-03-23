b60e51bf759ee3ded62fc74c8496b512652101e3
majoserra <mariajoseserra03@gmail.com>
2023-03-23 19:33:09 -0300

    Revert "opcion2"
    This reverts commit d5dc6035416ed215412ede0e89ce3c5376ed5bb8.

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,17 +1,11 @@
-
-#include<time.h>
-#include<stdlib.h>
-#include<stdio.h>
 #define N 20
-
 int main(){
     int i;
-    float vt[N];
-    printf("holaa");
+    double vt[N];
     for(i = 0;i<N; i++)
     {
         vt[i]=1+rand()%100;
-        printf("%.2f", vt[i]);
+        printf("%f ", vt[i]);
     }
     return 0;
 }
